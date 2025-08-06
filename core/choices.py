dadc98be4a43e98ca3660f6a6fec224ac28562d8
Luis Felipe <lfcvs.rj@gmail.com>
2025-08-06 20:01:29 -0300

    feat(core): create appointments choices

--- core/choices.py
+++ core/choices.py
@@ -5,3 +5,10 @@ class UserRole(models.TextChoices):
     ADMIN = "admin", "Admin"
     CLIENT = "client", "Client"
     BARBER = "barber", "Barber"
+
+
+class AppointmentStatus(models.TextChoices):
+    SCHEDULED = 'scheduled', 'Agendado'
+    COMPLETED = 'completed', 'Concluído'
+    CANCELED = 'canceled', 'Cancelado'
+    NO_SHOW = 'no_show', 'Não compareceu'
