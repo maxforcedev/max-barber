367f271e5448c3aa7d48bec703ec1e8944f476e3
Luis Felipe <lfcvs.rj@gmail.com>
2025-08-06 20:01:13 -0300

    feat(service): create appointments models

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from core.choices import AppointmentStatus
 
-# Create your models here.
+
+
+class Appointment(models.Model):
+    client = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='appointments')
+    barber = models.ForeignKey('barbers.Barber', on_delete=models.CASCADE, related_name='appointments')
+    service = models.ForeignKey('services.Service', on_delete=models.PROTECT)
+    
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()  # calculado no serializer
+    
+    status = models.CharField(
+        max_length=20,
+        choices=AppointmentStatus.choices,
+        default=AppointmentStatus.SCHEDULED
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
