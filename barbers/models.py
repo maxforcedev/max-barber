27372b3cd99e09d65aacfaa48a881f22fd01a93f
Luis Felipe <lfcvs.rj@gmail.com>
2025-08-06 19:40:02 -0300

    feat(barbers): add field services model barber

--- barbers/models.py
+++ barbers/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Barber(models.Model):
     user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, related_name='barber')
     photo = models.ImageField(blank=True, null=True)
+    services = models.ManyToManyField('services.Service', related_name='barbers')
