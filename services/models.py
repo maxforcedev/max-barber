6ec63bf0a04d65fc38a14c788bb6307a7b205cda
Luis Felipe <lfcvs.rj@gmail.com>
2025-08-06 19:37:36 -0300

    chore: Altery name service for services

---
+++ services/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=80)
+    detail = models.TextField(max_length=150, blank=True, null=True)
+    photo = models.ImageField(upload_to='services/', blank=True, null=True)
+    duration = models.PositiveIntegerField(help_text='Duração em minutos')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
