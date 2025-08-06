d52a15a201a90675f881d4843bfd99c87e7daec7
Luis Felipe <lfcvs.rj@gmail.com>
2025-08-06 20:17:51 -0300

    feat(appointment): create AppointmentAdmin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'client_name',
+        'barber_name',
+        'service',
+        'date',
+        'start_time',
+        'end_time',
+        'status',
+    )
+    list_filter = ('status', 'date', 'barber')
+    search_fields = ('client__name', 'barber__user__name')
+    ordering = ('-date', 'start_time')
+
+    def client_name(self, obj):
+        return obj.client.name
+
+    def barber_name(self, obj):
+        return obj.barber.user.name
--- appointments/models.py
+++ appointments/models.py
@@ -2,16 +2,15 @@ from django.db import models
 from core.choices import AppointmentStatus
 
 
-
 class Appointment(models.Model):
     client = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='appointments')
     barber = models.ForeignKey('barbers.Barber', on_delete=models.CASCADE, related_name='appointments')
     service = models.ForeignKey('services.Service', on_delete=models.PROTECT)
-    
+
     date = models.DateField()
     start_time = models.TimeField()
-    end_time = models.TimeField()  # calculado no serializer
-    
+    end_time = models.TimeField()
+
     status = models.CharField(
         max_length=20,
         choices=AppointmentStatus.choices,
@@ -19,4 +18,3 @@ class Appointment(models.Model):
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
-
