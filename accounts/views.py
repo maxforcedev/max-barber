5bdc53209d363afd2b25c19c00b8ce54a81c994e
Luis Felipe <lfcvs.rj@gmail.com>
2025-08-07 14:26:45 -0300

    feat(accounts): create views serializers

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LoginSerializer, SendLoginCodeSerializer
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SendLoginCodeView(APIView):
+    def post(self, request):
+        serializer = SendLoginCodeSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
